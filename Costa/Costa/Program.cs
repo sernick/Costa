79a729f2bf9ce7aafcfedaa0d1074b0c3559a457
Sergey Ivanov <sergey.nick.ivanov@gmail.com>
2017-04-13 21:40:46 +0300

    Проект для парсинга.

---
+++ Costa/Costa/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+
+namespace Costa
+{
+    internal class Program
+    {
+        #region Methods
+
+        private static void Main(string[] args)
+        {
+            const string path = @"C:\Users\ivanov\Desktop\Bournemouth-Chelsea.html";
+            string text = File.ReadAllText(path);
+        }
+
+        // https://ru.stackoverflow.com/questions/420354/Как-распарсить-html-в-net
+
+        #endregion
+    }
+}
